f5131f4113b99196125610bc2d79ff9de2df51a9
Mateusz-Koczorowski <mat.kocz00@gmail.com>
2021-05-01 22:04:51 +0200

    changes

--- api.py
+++ api.py
@@ -1,24 +1,52 @@
-import requests
 import csv
-import json
+import sys
+import requests
 
 url = "https://api.spacexdata.com/v3/launches"
-result = requests.get(url)
-result = result.json()
-head_line = ['flight_number', 'mission_name' , 'rocket_id' , 'rocket_name' , 'launch_date_utc' , 'video_link']
-agree = bool(input("Do you want to write header line in file (usefull to import data into excel or mysql)? Leave blank if not, enter anything if so: "))
 
-with open("flights_spacex.csv", mode="w", newline='') as spacex_file:
-    csv_write = csv.writer(spacex_file)
-    if agree is True:
-        csv_write.writerow(head_line)
-    for row in result:
-        flight_number = row['flight_number']
-        mission_name = row['mission_name']
-        rocket_id = row['rocket']['rocket_id']
-        rocket_name = row['rocket']['rocket_name']
-        launch_date_utc = row['launch_date_utc']
-        video_link = row['links']['video_link']
-        list_to_write = [flight_number, mission_name, rocket_id, rocket_name, launch_date_utc, video_link]
-        print(list_to_write)
-        csv_write.writerow(list_to_write)
+
+class OpenFile():
+    head_line = ['flight_number', 'mission_name', 'rocket_id', 'rocket_name', 'launch_date_utc', 'video_link']
+
+    def __init__(self, url):
+        self.url = url
+
+    def fetch_spacex(self):
+        try:
+            result = requests.get(url)
+            result.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise print("Fail to fetch data!")
+        return result.json()
+
+    def __enter__(self):
+        self.spacex_file = open('flights_spacex.csv', mode='w', newline='')
+        print("Opening file!")
+
+    def run(self):
+        self.data = self.fetch_spacex()
+        self.csv_write = csv.writer(self.spacex_file)
+        if len(sys.argv) > 1:
+            start_arg = sys.argv[1]
+            if start_arg == "-h":
+                self.csv_write.writerow(self.head_line)
+        for row in self.data:
+            flight_number = row.get('flight_number')
+            mission_name = row.get('mission_name')
+            rocket_id = row.get('rocket').get('rocket_id')
+            rocket_name = row.get('rocket').get('rocket_name')
+            launch_date_utc = row.get('launch_date_utc')
+            video_link = row.get('links').get('video_link')
+            list_to_write = [flight_number, mission_name, rocket_id, rocket_name, launch_date_utc, video_link]
+            print(list_to_write)
+            self.csv_write.writerow(list_to_write)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.spacex_file.close()
+        print("Closing file!")
+
+
+spacex = OpenFile(url)
+
+with spacex:
+    spacex.run()
